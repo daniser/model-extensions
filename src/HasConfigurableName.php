bebc657a1fd06abb647ede3d71018f0c480ef770
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2020-05-19 18:31:27 +0200

    Initial commit

---
+++ src/HasConfigurableName.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Daniser\ModelExtensions\Concerns;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
+
+trait HasConfigurableName
+{
+    public function getNameSource(): ?string
+    {
+        return $this->nameSource ?? null;
+    }
+
+    protected function initializeHasConfigurableName(): void
+    {
+        foreach ($this->suggestNameSources() as $nameSource) {
+            if ($table = Config::get($nameSource)) {
+                $this->setTable($table);
+                return;
+            }
+        }
+    }
+
+    private function suggestNameSources(): array
+    {
+        $suggestions = ['database.model_table_mapping.'.__CLASS__];
+
+        if ($nameSource = $this->getNameSource()) {
+            $suggestions[] = $nameSource;
+        } elseif (count($components = explode('\\', __CLASS__)) > 2 || $components[0] !== 'App') {
+            $suggestions[] = Str::kebab($components[1]).'.'.Str::snake(end($components).'_table');
+        }
+
+        return $suggestions;
+    }
+}
---
+++ src/HasUuidPrimaryKey.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Daniser\ModelExtensions\Concerns;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+trait HasUuidPrimaryKey
+{
+    public function hasOrderedUuid(): bool
+    {
+        return (bool) $this->orderedUuid;
+    }
+
+    protected static function bootHasUuidPrimaryKey(): void
+    {
+        static::creating(function (Model $model) {
+            $uuid = $model->hasOrderedUuid()
+                ? (string) Str::orderedUuid()
+                : (string) Str::uuid();
+
+            $model->setAttribute($model->getKeyName(), $uuid);
+        });
+    }
+
+    protected function initializeHasUuidPrimaryKey(): void
+    {
+        $this->setKeyType('string')->setIncrementing(false);
+    }
+}
